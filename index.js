20b021466e24e45309a8ab8fc04196af08387333
Icemic <bingfeng.jw@gmail.com>
2017-07-15 13:04:00 +0800

    code optimization

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const PIXI = require('pixi.js');
-import './BetterTextRenderer';
-// import TinySDF from './lib/SDF';
-import { parseRichText } from './lib/utils';
-import TextStyle from './lib/TextStyle';
-// import { getSDFTexture, getTextData, getSDFConfig } from './lib/SDFCache';
+
+import RichText from './lib/RichText';
 
 PIXI.settings.RENDER_OPTIONS.antialias = true;
 var app = new PIXI.Application({
@@ -14,57 +11,6 @@ var app = new PIXI.Application({
 
 app.renderer.backgroundColor = 0xffffff;
 
-// const sdfConfig = getSDFConfig();
-
-class BetterText extends PIXI.Sprite {
-  constructor(text, style = {}) {
-    super();
-
-    this.style = new TextStyle(style);
-
-    this._text = text;
-
-    this.vertexList = [];
-    this.needUpdateCharacter = true;
-
-    /**
-     * Plugin that is responsible for rendering this element.
-     * Allows to customize the rendering process without overriding '_renderWebGL' & '_renderCanvas' methods.
-     *
-     * @member {string}
-     * @default 'sprite'
-     */
-    this.pluginName = 'bettertext';
-
-  }
-  set text(value) {
-    if (this._text !== value) {
-      this._text = value;
-      this.needUpdateCharacter = true;
-    }
-  }
-  get text() {
-    return this._text;
-  }
-  _renderWebGL(renderer) {
-    const gl = renderer.gl;
-    // gl.getExtension("OES_standard_derivatives");
-    // console.log(gl)
-    gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE);
-    gl.enable(gl.BLEND);
-
-    if (this.needUpdateCharacter) {
-      // this.generateCharacterData();
-      this.vertexList = parseRichText(this._text, this);
-      this.needUpdateCharacter = false;
-    }
-
-    renderer.setObjectRenderer(renderer.plugins[this.pluginName]);
-    renderer.plugins[this.pluginName].render(this);
-  }
-}
-
-
 // var chars = `我与父亲不相见已二年余了，我最不能忘记的是他的背影。那年冬天，祖母死了，父亲的差使也交卸了，正是祸不单行的日子，我从北京到徐州，打算跟着父亲奔丧回家。到徐州见着父亲，看见满院狼藉的东西，又想起祖母，不禁簌簌地流下眼泪。父亲说，“事已如此，不必难过，好在天无绝人之路！”
 // 　　回家变卖典质，父亲还了亏空；又借钱办了丧事。这些日子，家中光景很是惨淡，一半为了丧事，一半为了父亲赋闲。丧事完毕，父亲要到南京谋事，我也要回北京念书，我们便同行。
 // 　　到南京时，有朋友约去游逛，勾留了一日；第二日上午便须渡江到浦口，下午上车北去。父亲因为事忙，本已说定不送我，叫旅馆里一个熟识的茶房陪我同去。他再三嘱咐茶房，甚是仔细。但他终于不放心，怕茶房不妥帖；颇踌躇了一会。其实我那年已二十岁，北京已来往过两三次，是没有甚么要紧的了。他踌躇了一会，终于决定还是自己送我去。我两三回劝他不必去；他只说，“不要紧，他们去不好！”
@@ -73,7 +19,7 @@ class BetterText extends PIXI.Sprite {
 // 　　近几年来，父亲和我都是东奔西走，家中光景是一日不如一日。他少年出外谋生，独力支持，做了许多大事。那知老境却如此颓唐！他触目伤怀，自然情不能自已。情郁于中，自然要发之于外；家庭琐屑便往往触他之怒。他待我渐渐不同往日。但最近两年的不见，他终于忘却我的不好，只是惦记着我，惦记着我的儿子。我北来后，他写了一信给我，信中说道，“我身体平安，惟膀子疼痛利害，举箸提笔，诸多不便，大约大去之期不远矣。”我读到此处，在晶莹的泪光中，又看见那肥胖的，青布棉袍，黑布马褂的背影。唉！我不知何时再能与他相见！`;
 const chars = '泽材灭逐莫笔亡鲜词test圣择寻厂睡博';
 
-var text = new BetterText(chars);
+var text = new RichText(chars);
 app.stage.addChild(text);
 text.style.fontSize = 48;
 text.style.strokeEnable = false;
@@ -102,17 +48,17 @@ app.stage.addChild(text2);
 // window.text = text;
 
 
-import { getSDFTexture, getTextData, getSDFConfig } from './lib/SDFCache';
+// import { getSDFTexture, getTextData, getSDFConfig } from './lib/SDFCache';
 
-var sprite = new PIXI.Sprite(new PIXI.Texture(getSDFTexture()));
-sprite.x = -0;
-sprite.y = 200;
-app.stage.addChild(sprite)
+// var sprite = new PIXI.Sprite(new PIXI.Texture(getSDFTexture()));
+// sprite.x = -0;
+// sprite.y = 200;
+// app.stage.addChild(sprite)
 
 // var image = new PIXI.Sprite.fromImage('img.png');
 // app.stage.addChild(image);
 
-let i = 0;
+// let i = 0;
 
 // setInterval(() => {
 //   const length = (Math.random() * 10) << 0;
